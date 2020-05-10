18b26b1db3db3d378c166d25d9bf27a1af75aae2
Revanth Kumar <gamingkid45@gmail.com>
2020-05-10 07:46:33 +0530

    'done'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Enter Number</h1>
+    <p>Only integers in below</p>
+    <input v-model='input_number' type='text' />
+    <br/>
+    <button v-on:click='input_handle'>Get Cubed </button>
+    <h2 v:if="answer !== null">The Cube of the Number is {{answer}}</h2>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        input_number : null,
+        answer: null
+      }
+    },
+    methods: {
+      input_handle: function() {
+        let n = Number(this.input_number)
+        if (!isNaN(n) && n!==0) {
+          this.answer = n*n*n
+        }
+        else {
+          alert("Please enter Integers Only.")
+        }
+        this.input_number = null
+      }
+    }
+
+    //posibly Life Cycles
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+   * {
+    text-align: center;
+   }
+
+   p {
+    color: grey;
+   }
+
+   h1 {
+    font-weight: bold;
+   }
+
+   input {
+    padding: 1em;
+    font-size: 2em;
+    border: 4px solid grey;
+   }
+
+   button {
+    margin: 1em;
+    padding: 1em;
+    font-size: 2em;
+    border: 2px solid skyblue;
+    border-radius: 60px;
+   }
 </style>
